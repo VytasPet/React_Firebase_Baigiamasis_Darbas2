f3f1e1b4d29b86bb43132ceeabe38b1bb16e7f12
Vytautas P <v.petrucionis@gmail.com>
2023-04-20 11:24:11 +0300

    Pages, header created

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Welcome to Login Page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/ShopsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShopsPage() {
+  return (
+    <div>
+      <h1>Welcome to Shops Page</h1>
+    </div>
+  );
+}
+
+export default ShopsPage;
